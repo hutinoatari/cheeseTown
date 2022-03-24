dbb9302bb2faba853e00cc29207b2f817e585a8d
hutinoatari <63452799+hutinoatari@users.noreply.github.com>
2022-03-25 03:34:37 +0900

    switch文への変更と山括弧のエスケープ

--- cheeseTown.ts
+++ cheeseTown.ts
@@ -1,18 +1,27 @@
-const regex = /^\[(?<tag>title|section|subsection|subsubsection)\] (?<text>.+)/;
+const regex = /^\[(.+)\] (.+)/;
+const escape = (text: string): string =>
+  text.trim().replace(/[<>]/g, (c) => c == "<" ? "&lt;" : "&gt;");
 
 const compile = (markdown: string): string => {
   const plainElements: string[] = markdown.split("\n").map((e) => e.trim())
     .filter((e) => e !== "");
   const htmlElements: string[] = plainElements.map((e) => {
     const match = e.match(regex);
-    if (match === null) return `<p>${e}</p>`;
-    const tagName = match.groups?.tag ?? "";
-    const text = match.groups?.text ?? "";
-    if (tagName === "title") return `<h1>${text}</h1>`;
-    if (tagName === "section") return `<h2>${text}</h2>`;
-    if (tagName === "subsection") return `<h3>${text}</h3>`;
-    if (tagName === "subsubsection") return `<h4>${text}</h4>`;
-    return "";
+    if (match === null) return `<p>${escape(e)}</p>`;
+    const tagName = match[1];
+    const text = escape(match[2]);
+    switch (tagName) {
+      case "title":
+        return `<h1>${text}</h1>`;
+      case "section":
+        return `<h2>${text}</h2>`;
+      case "subsection":
+        return `<h3>${text}</h3>`;
+      case "subsubsection":
+        return `<h4>${text}</h4>`;
+      default:
+        return `<p>${escape(e)}</p>`;
+    }
   });
   const html: string = htmlElements.join("");
   return html;
